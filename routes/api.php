c03a804aa1d26bb4931b4b16fefb4cc699908d7c
work <temogogitidze@redberry.ge>
2023-05-04 23:21:17 +0400

    feat(payment): add payment request

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/payment', [PaymentController::class]);
+Route::post('/payment', PaymentController::class);
